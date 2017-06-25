b9cb43bdac220b970443b97932b46a46159b474e
“zojian” <hellozojian@gmail.com>
2017-06-25 23:55:13 +0800

    下载图片按用户名新建文件夹，下载高清图

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import re
 import os
+import sys
 
 
 def get_user_info(username):
@@ -28,45 +29,75 @@ def get_user_media(username):
           '=============================')
 
     url = 'https://www.instagram.com/graphql/query/'
+
     payload = {'query_id': 17880160963012870,
                'id': user_id,
                'first': media_num}
+
     r = requests.get(url, params=payload)
     return re.findall('"thumbnail_src": "(.*?)",', r.text)
 
 
-def save_image(list_image):
+def save_image(list_image, username):
     """
     保存照片到本地
     :param list_image:
     :return:
     """
 
+    header = {
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36'
+                      ' (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+        'accept': '*/*',
+        'accept-encoding': 'gzip, deflate, sdch, br'
+    }
 
+    if os.path.exists(username):
+        os.chdir(username)
+    else:
+        os.mkdir(username)
+        os.chdir(username)
+
+    num = 1
 
     for url in list_image:
-        # new_url = re.findall('http.*?/t51.2885-15/', url)[0] + re.findall('/e.*?/(.*?.jpg)', url)[0]
+        new_url = re.findall('http.*?/t51.2885-15/', url)[0] + re.findall('/e.*?/(.*?.jpg)', url)[0]
 
-        print('正在下载:'+url)
+        print('正在下载第:'+str(num)+'张图片')
 
-        file_name = re.findall('t51.2885-15/(.*)', url)
+        num += 1
+
+        file_name = re.findall('t51.2885-15/(.*)', new_url)
 
         print('文件名：'+file_name[0].replace("/", "_"))
 
         file = open(file_name[0].replace("/", "_"), 'wb')
 
-        r = requests.get(url, timeout=5)
+        # FIXME: 爬到一定实现出现错误：requests.exceptions.ConnectionError: HTTPSConnectionPool(host='ig-s-c-a.akamaihd.net', port=443): Read timed out.
 
-        print('打开成功 ！')
+        r = requests.get(new_url, headers=header, timeout=5)
+
+        # FIXME: 保存的照片都是正方形
 
         file.write(r.content)
         file.close()
-        # print(file_name[0].replace("/", "_")+'download success!')
+        print('下载成功')
 
 
 def main():
-    # print(get_user_media('zojian'))
-    save_image(get_user_media('zojian'))
+
+    if os.path.exists('download'):
+        os.chdir('download')
+    else:
+        os.mkdir('download')
+        os.chdir('download')
+
+    save_image(get_user_media(sys.argv[1]), sys.argv[1])
+
+    print('=============================\n' +
+          '全部下载完成\n'
+          '=============================\n' +)
+
 
 if __name__ == "__main__":
     main()
